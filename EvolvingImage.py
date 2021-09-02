190a8863f7ac03f8e078d9a039bcdfef4a145de4
halifk <aasadi.ai.work@gmail.com>
2021-09-01 22:40:27 -0400

    Finished implementation of GA, training loss not decreasing!

--- EvolvingImage.py
+++ EvolvingImage.py
@@ -1,15 +1,21 @@
 import numpy as np
 from numpy import random
 from PIL import Image
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 class EvolvingImage:
-    def __init__(self,referenceImgPath,imgDim=(30,30),populationSize=10,crossoverRate=1,mutationRate=0.03):
+    def __init__(self,referenceImgPath,imgDim=(30,30),populationSize=30,crossoverRate=0.5,mutationRate=0.03):
         self.imgDim = imgDim
+        self.populationSize = populationSize
         self.population = [random.randint(0,2,imgDim[0]*imgDim[1]) for i in range(populationSize)]
         self.referenceImgArray = self.downSampleImg(referenceImgPath)
         self.referenceFlattened = self.referenceImgArray.flatten()
         self.crossoverRate = crossoverRate
         self.mutatationRate = mutationRate
+        self.generationNumber = 0
+        self.topPerformer = None
+        self.fitnessRecord = []
     
     def downSampleImg(self,imgPath):
         #convert to greyscale, downsample to targetDimension,quantize to 1,0
@@ -19,7 +25,7 @@ class EvolvingImage:
         return quantizedImageArray.astype(int)
 
     def fitness(self,X):
-        return 1/np.sum(np.logical_xor(X,self.referenceFlattened))
+        return 1/(np.sum(np.logical_xor(X,self.referenceFlattened))+0.00001)
 
     def crossover(self,X,Y):
         crossoverPoint = int(np.random.random()*(len(X)-1))
@@ -35,11 +41,36 @@ class EvolvingImage:
     def mate(self,X,Y):
         if np.random.random()<=self.crossoverRate:
             return self.crossover(X,Y)
-        return (X,Y)
+        return [X,Y]
 
     def getMatingPool(self):
         fitnessScores = [self.fitness(X) for X in self.population]
+        self.topPerformer = self.population[np.argmax(fitnessScores)]
+        self.fitnessRecord.append(np.mean(fitnessScores))
         normalizedFitnessScores = [fitness/np.sum(fitnessScores) for fitness in fitnessScores]
+        return np.random.default_rng().choice(a=self.population,size=self.populationSize,p=normalizedFitnessScores)
+
+    def breed(self):
+        matingPool = self.getMatingPool()
+        poolSize = len(matingPool)
+        nextGeneration = []
+        for i in range(self.populationSize//2):
+            offSpring = self.mate(matingPool[np.random.randint(0,poolSize)],matingPool[np.random.randint(0,poolSize)])
+            nextGeneration.append(offSpring[0])
+            nextGeneration.append(offSpring[1])
+
+        self.population = nextGeneration
+        self.generationNumber+=1
+    
+    def n_Steps(self,n):
+        for i in tqdm(range(n)):
+            self.breed()
+    
         
-        
-test = EvolvingImage("mickeyMouse.jpg")
+# test = EvolvingImage("mickeyMouse.jpg")
+# test.n_Steps(10000)
+# plt.plot(test.fitnessRecord)
+# plt.show()
+# # img = Image.fromarray(np.reshape(test.topPerformer*255,test.imgDim))
+# # img = img.resize((1000,1000))
+# # img.show()
