5c0173300816f3f10e91ed258f96939ad69cd21e
halifk <aasadi.ai.work@gmail.com>
2021-09-03 13:58:05 -0400

    Adds tournament selection,prevents premature convergence

--- EvolvingImage.py
+++ EvolvingImage.py
@@ -1,11 +1,12 @@
+#%%
 import numpy as np
 from numpy import random
 from PIL import Image
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-
+#%%
 class EvolvingImage:
-    def __init__(self,referenceImgPath,imgDim=(15,15),populationSize=100,crossoverRate=0.5,mutationRate=0.03):
+    def __init__(self,referenceImgPath,imgDim=(30,30),populationSize=200,crossoverRate=0.5,mutationRate=0.005,crossoverPercentLength=0.05,selectionMethod="tournament"):
         self.imgDim = imgDim
         self.populationSize = populationSize
         self.population = [random.randint(0,2,imgDim[0]*imgDim[1]) for i in range(populationSize)]
@@ -16,6 +17,8 @@ class EvolvingImage:
         self.generationNumber = 0
         self.topPerformer = None
         self.fitnessRecord = []
+        self.crossoverLength = int(imgDim[0]*imgDim[1]*crossoverPercentLength)
+        self.selectionMethod = {"tournament":self.tournamentSelection,"roulette":self.rouletteSelection}[selectionMethod]
     
     def downSampleImg(self,imgPath):
         #convert to greyscale, downsample to targetDimension,quantize to 1,0
@@ -29,8 +32,8 @@ class EvolvingImage:
 
     def crossover(self,X,Y):
         crossoverPoint = int(np.random.random()*(len(X)-1))
-        X_new = np.concatenate((X[:crossoverPoint],Y[crossoverPoint:]))
-        Y_new = np.concatenate((Y[:crossoverPoint],X[crossoverPoint:]))
+        X_new = np.concatenate((X[:crossoverPoint],Y[crossoverPoint:crossoverPoint+self.crossoverLength],X[crossoverPoint+self.crossoverLength:]))
+        Y_new = np.concatenate((Y[:crossoverPoint],X[crossoverPoint:crossoverPoint+self.crossoverLength],Y[crossoverPoint+self.crossoverLength:]))
         return [self.mutatate(X_new),self.mutatate(Y_new)]
 
     def mutatate(self,X):
@@ -43,16 +46,29 @@ class EvolvingImage:
             return self.crossover(X,Y)
         return [X,Y]
 
-    def getMatingPool(self):
+    def rouletteSelection(self):   
         fitnessScores = [self.fitness(X) for X in self.population]
         self.topPerformer = self.population[np.argmax(fitnessScores)]
         self.fitnessRecord.append(np.mean(fitnessScores))
         normalizedFitnessScores = [fitness/np.sum(fitnessScores) for fitness in fitnessScores]
         return np.random.default_rng().choice(a=self.population,size=self.populationSize,p=normalizedFitnessScores)
 
+    def tournamentSelection(self,tournamentSize=2):
+        fitnessScores = [self.fitness(X) for X in self.population]
+        self.topPerformer = self.population[np.argmax(fitnessScores)]
+        self.fitnessRecord.append(np.mean(fitnessScores))
+        matingPool = []
+        for i in range(self.populationSize):
+            loc0 = np.random.randint(0,self.populationSize)
+            loc1 = np.random.randint(0,self.populationSize)
+            if fitnessScores[loc0]>=fitnessScores[loc1]:
+                matingPool.append(self.population[loc0])
+            else:
+                matingPool.append(self.population[loc1])
+        return matingPool
+
     def breed(self):
-        matingPool = self.getMatingPool()
-        matingPool[0]=self.topPerformer
+        matingPool = self.selectionMethod()
         poolSize = len(matingPool)
         nextGeneration = []
         for i in range(self.populationSize//2):
@@ -66,13 +82,22 @@ class EvolvingImage:
     def n_Steps(self,n):
         for i in tqdm(range(n)):
             self.breed()
+
+    def tuneParams(self):
+        pass
     
     def displayRecord(self):
-        plt.plot(self.fitnessRecord)
+        plt.plot(self.fitnessRecord,label="Fitness")
+        #plt.plot(self.fitness(self.referenceFlattened),label="Global Optimum")
         plt.title("Training Results")
         plt.xlabel("Generation Number")
         plt.ylabel("Fitness")
+        #plt.legend()
         plt.show()
+#%%
 
-        
-
+test = EvolvingImage("mickeyMouse.jpg")
+test.n_Steps(3000)
+test.displayRecord()
+Image.fromarray(np.reshape(test.topPerformer*255,test.imgDim)).resize((1000,1000)).show()
+Image.fromarray(test.referenceImgArray*255).resize((1000,1000)).show()
